f77a7f6cc4b3e0c7a9ced5ab527bf2d1acc705f1
Jon-Nutman <jonnut730@gmail.com>
2021-03-02 17:48:57 -0500

    almost

--- hw.sql
+++ hw.sql
@@ -50,3 +50,59 @@ WHERE title LIKE '%Death%' AND genres LIKE '%Comedy%';
 SELECT * FROM movies
 WHERE title LIKE '%super%' 
 AND (title LIKE '%(2001)' OR title LIKE '%(2002)');
+
+-- question 12 -- 
+
+SELECT m.title, r.rating
+FROM movies m
+JOIN ratings r ON m.id = r.movie_id
+WHERE title='Godfather, The (1972)';
+
+-- question 13 -- 
+
+SELECT m.title, r.rating
+FROM movies m
+JOIN ratings r ON m.id = r.movie_id
+WHERE title='Godfather, The (1972)'
+ORDER BY r.timestamp DESC;
+
+-- question 14 -- 
+
+SELECT l.imdb_id, m.title
+FROM links l
+LEFT JOIN movies m ON m.id = l.movie_id
+WHERE title LIKE '%(2005)'
+AND genres LIKE '%Comedy%';
+
+-- question 15 --
+SELECT * FROM movies
+JOIN ratings r ON movies.id = r.movie_id
+WHERE rating IS NULL;
+
+-- question 16 --
+
+SELECT AVG(rating) FROM ratings
+WHERE movie_id=1;
+
+-- question 17 --
+
+SELECT COUNT(rating) FROM ratings
+WHERE movie_id=1;
+
+-- question 18 --
+
+SELECT COUNT(id) FROM movies
+WHERE genres LIKE '%Comedy%';
+
+-- question 19 --
+
+SELECT AVG(rating) FROM ratings
+WHERE user_id=1;
+
+-- question 20 --
+SELECT COUNT(user_id) FROM ratings
+WHERE user_id = 1;
+
+
+-- question 21 --
+
